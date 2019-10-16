9dec386f94209e8f7c7ed14d13924a4bf70d785b
Jason Wu <zwu047@hotmail.com>
2019-10-17 11:10:05 +1300

    fix issue and added comments

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,10 +2,13 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>{{message}}</p>
+    <!-- test v-bind, howover your mouse -->
     <p v-bind:title="message1">Test v-bind function</p>
-    <input type="bool" v-model="message1">
-    <br>
+    <!-- redirection model -->
+    <input type="text" v-model="message">
+    <!-- test bool  -->
     <p v-if="true">Weather is good</p>
+    <!-- function to reverse the string -->
     <span @click="res">{{message}}</span>
 
     <p v-for="(item, index) in list"
@@ -20,10 +23,11 @@
 
 <script>
 export default {
+
   data() {
     return {
       message: 'Good Morning, Jason !!!',
-      message1: `New Zealand${new Date().toLocaleDateString}`,
+      message1: `New Zealand${new Date().toLocaleString()}`,
       list: ['123', '456', '789'],
       bool: true,
     };
